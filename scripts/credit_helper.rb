6f30df7155f06bface32a87a43d51bfe3698f1ff
Jkolber <jkolber393@gmail.com>
2015-09-25 21:09:38 -0700

    Reorganization of files

--- scripts/credit_helper.rb
+++ scripts/credit_helper.rb
@@ -11,18 +11,15 @@ begin
 	db.execute('UPDATE Lights SET credits=credits-1 WHERE credits>0;');
 	labels = db.execute('SELECT label FROM Lights where credits<=0;')
 	labels.each do |label|
-		puts "label: #{label[0]}"
+		puts "#{Time.now}: label: #{label[0]}"
 		lights = lifx.lights.with_label(label[0])
-		puts "lights: #{lights}"
+		puts "#{Time.now}: lights: #{lights}"
 		lights.turn_off!
-	end
-	open('credit_log.log', 'a') do |log|
-		log.puts "#{Time.now}"
-		log.puts "\n"
 	end
 	db.close if db
 rescue SQLite3::Exception => e
-	open('credit_log.log', 'a') do |log|
+	chdir('../logs')
+	open('credit.log', 'a') do |log|
 		log.puts "#{Time.now}: "
 		log.puts e
 		log.puts "\n"
